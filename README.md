7a308080d271e4b74269998d4b0b96abecd089bb
kaninjegarn <propaladin@hotmail.com>
2021-02-24 09:24:51 +0100

    inital commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+###
+created by using create-react app
+
+###
+start command -> npm start
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { GiphyList, Header } from "./components";
+
+export default () => {
+  return (
+    <div>
+      <Header />
+      <GiphyList />
+    </div>
+  );
+}
